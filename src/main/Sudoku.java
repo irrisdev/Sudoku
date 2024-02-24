1fddea0c3485a85ad4aff7e9c24dcc5dcc8b5f23
root <root@debian>
2024-02-24 17:09:46 +0000

    Pre Removal of String output at checkBox

--- src/main/Sudoku.java
+++ src/main/Sudoku.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -15,8 +16,11 @@ public class Sudoku {
 	 * 
 	 */
 	//private static int[][] board = new int[9][9];
-
-	private static int[][] board = {
+	public static void main(String[] args) {
+		System.out.println(validate());
+	}
+	
+	private static int[][] mainBoard = {
 			{5, 3, 4, 6, 7, 8, 9, 1, 2},
 		    {6, 7, 2, 1, 9, 5, 3, 4, 8},
 		    {1, 9, 8, 3, 4, 2, 5, 6, 7},
@@ -27,40 +31,39 @@ public class Sudoku {
 		    {2, 8, 7, 4, 1, 9, 6, 3, 5},
 		    {3, 4, 5, 2, 8, 6, 1, 7, 9}
         };
-	
-	public static void main(String[] args) {
+	/* 	3x3 Test Case
+	    {1,2,3,4,5,6,7,8,9},
+		{9,1,2,3,4,5,6,7,8},
+		{8,9,1,2,3,4,5,6,7},
+		{7,8,9,1,2,3,4,5,6},
+		{6,7,8,9,1,2,3,4,5},
+		{5,6,7,8,9,1,2,3,4},
+		{4,5,6,7,8,9,1,2,3},
+		{3,4,5,6,7,8,9,1,2},
+		{2,3,4,5,6,7,8,9,1}
+	 */
+	private static boolean validate() {
 		
-		//fillBoardEmpty(board);
-		validate(board);
-		
-	}
-	
-	
-
-	private static boolean validate(int[][] inputBoard) {
 		boolean valid = true;
 		List<Integer> curRow, curCol;
-		
 		int overall=0;
+		
 		do {
-			
+			//Loop for each cell - 81
 			for (int i = 0; i < 9; i++) {
 				for (int j = 0; j < 9 && valid; j++) {
 					
-					int cellValue = inputBoard[i][j];
-					
-					curRow = Arrays.stream(inputBoard[i]).boxed().toList();
-					curCol = Arrays.stream(inputBoard[j]).boxed().toList();
-					
-					int occurance = Collections.frequency(curRow, cellValue) + Collections.frequency(curCol, cellValue);
-					
-					if(occurance > 2 || inBox(cellValue, i, j)) {
-						valid = false;
-					}
+					int cellValue = mainBoard[i][j];
+					//Get the row and column that the cell belongs to
+					curRow = Arrays.stream(mainBoard[i]).boxed().toList();
+					curCol = Arrays.stream(mainBoard[j]).boxed().toList();
+					//Count number of occurrences cell value has
+					int occurrence = Collections.frequency(curRow, cellValue) + Collections.frequency(curCol, cellValue);
+					//Call function to check if value is already in 3x3
+					if(occurrence > 2 || inBox(cellValue, i, j)) {valid = false;}
 					overall++;
 				}
 			}
-			
 		} while (valid && overall < 81);
 		
 		return valid;
@@ -79,24 +82,29 @@ public class Sudoku {
 		output += "Value: " + cellValue + " Index: " + (row+1)*(col+1) + " Grid: " + (checkRow*checkCol) + "\n-----------------------------\n";
 				
 		String coord = "";
-		
+		List<Integer> seen = new ArrayList<>();
 		for (int i = (checkRow*3)-3; i < checkRow*3; i++) {
 			
 			String coordsChecked = "";
 			String numbChecked = "";
 			
 			for (int j = (checkCol*3)-3; j < checkCol*3; j++) {
-				
+
 				coordsChecked += "(" + j + ", " + i + ")";
-				numbChecked += cellValue + " ";
+				numbChecked += mainBoard[i][j] + " ";
+				
+				if(Collections.frequency(seen, cellValue) > 1) {
+					System.out.println("Error");
+					return true;
+				} else { seen.add(mainBoard[i][j]);}
+				
 			}
 			
 			coord += coordsChecked + "   " + numbChecked + "\n";
 
 		}
 		output += coord;
-		System.out.println(output);
-		
+		//System.out.println(output);
 		return false;
 	}
 
@@ -120,8 +128,8 @@ public class Sudoku {
 				for (int j = 0; j < 9; j++) {
 					int unique = 0;
 
-					curRow = Arrays.stream(board[i]).boxed().toList();
-					curCol = Arrays.stream(board[j]).boxed().toList();
+					curRow = Arrays.stream(mainBoard[i]).boxed().toList();
+					curCol = Arrays.stream(mainBoard[j]).boxed().toList();
 					System.out.println("Unique = " + Integer.toString(unique));
 					int rand = random.nextInt(9) + 1;
 					while (curRow.contains(rand) || curCol.contains(rand)) {
@@ -136,7 +144,7 @@ public class Sudoku {
 					}
 					
 					if (valid) {
-						board[i][j] = rand;
+						mainBoard[i][j] = rand;
 					}
 					overall++;
 					System.out.println(overall);
@@ -146,7 +154,7 @@ public class Sudoku {
 		}
 		
 		System.out.println(valid);
-		return board;
+		return mainBoard;
 	}
 	
 	//Given a int[][] board = new int[9][9] this method fills the board to 0's
